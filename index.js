7b3b2a1bb1d1b1601dc7d02d49207ded9de9134b
David Romero <120739245+devPrueba@users.noreply.github.com>
2023-12-21 23:45:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Te Amo...Alejandra\n O En el idioma de Marvel \nIron Man: Te amo 3 millones\nDoctor Strange: Te Amo en todos los universos\nThor: Tu me volviste digno\nSpider-Man: Tu eres mi Camino\nBruja Escarlata: Eres mi tristeza y mi esperanza pero sobre todo mi amor\nDeadpool: Tus locuras van con las mias\nMj: Aqui estoy no me voy a ningun lado')
+    alert('Te Quiero...Karen Grabriela\n O En el idioma de Marvel \nIron Man: Te amo 3 millones\nDoctor Strange: Te Amo en todos los universos\nThor: Tu me volviste digno\nSpider-Man: Tu eres mi Camino\nBruja Escarlata: Eres mi tristeza y mi esperanza pero sobre todo mi amor\nDeadpool: Tus locuras van con las mias\nMj: Aqui estoy no me voy a ningun lado')
     alert('Propuesta\nYo no vengo a ofrecerle la luna y las estrella eso se lo ofrece cualquiera.\nYo le ofrezco mis manos para sostenerla, mis labios para que descanse, mis sueños curiosos y perversos, unos brazos largos y honestos, un pecho que le sirve de almohada y un amor más allá de la esperanza.')
 });
 

9aa8f82ca83a7aa0ee4b8faa0fda30282435848b
David Romero <120739245+devPrueba@users.noreply.github.com>
2023-12-23 16:31:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Te Quiero...Karen Grabriela\n O En el idioma de Marvel \nIron Man: Te amo 3 millones\nDoctor Strange: Te Amo en todos los universos\nThor: Tu me volviste digno\nSpider-Man: Tu eres mi Camino\nBruja Escarlata: Eres mi tristeza y mi esperanza pero sobre todo mi amor\nDeadpool: Tus locuras van con las mias\nMj: Aqui estoy no me voy a ningun lado')
-    alert('Propuesta\nYo no vengo a ofrecerle la luna y las estrella eso se lo ofrece cualquiera.\nYo le ofrezco mis manos para sostenerla, mis labios para que descanse, mis sueños curiosos y perversos, unos brazos largos y honestos, un pecho que le sirve de almohada y un amor más allá de la esperanza.')
+    alert('Te Quiero...Karen Grabriela\n O En el idioma de Marvel \nTe amo 3 millones\nTe Amo en todos los universos\nTu me volviste digno\n Tu eres mi Camino\n Eres mi tristeza y mi esperanza pero sobre todo mi amor\nTus locuras van con las mias\n Aqui estoy no me voy a ningun lado')
+    alert('Propuesta\nYo no vengo a ofrecerle la luna y las estrella eso se lo ofrece cualquiera.\nYo le ofrezco mis manos para sostenerla, mis labios para que descanse, mis sueños curiosos y perversos, unos brazos largos y honestos, un pecho que le sirve de almohada y un amor más allá de la esperanza...')
 });
 
 const noBtn = document.querySelector('#noBtn');

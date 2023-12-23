8f2a06196357227169d9ff6f3525b5f660ad3bea
David Romero <120739245+devPrueba@users.noreply.github.com>
2023-12-23 16:32:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Te Quiero...Karen Grabriela\n O En el idioma de Marvel \nTe amo 3 millones\nTe Amo en todos los universos\nTu me volviste digno\n Tu eres mi Camino\n Eres mi tristeza y mi esperanza pero sobre todo mi amor\nTus locuras van con las mias\n Aqui estoy no me voy a ningun lado')
+    alert('Te Quiero...Decir Con el lenguaje de mi corazon\n O En el idioma de Marvel \nTe amo 3 millones\nTe Amo en todos los universos\nTu me volviste digno\n Tu eres mi Camino\n Eres mi tristeza y mi esperanza pero sobre todo mi amor\nTus locuras van con las mias\n Aqui estoy no me voy a ningun lado')
     alert('Propuesta\nYo no vengo a ofrecerle la luna y las estrella eso se lo ofrece cualquiera.\nYo le ofrezco mis manos para sostenerla, mis labios para que descanse, mis sueños curiosos y perversos, unos brazos largos y honestos, un pecho que le sirve de almohada y un amor más allá de la esperanza...')
 });
 
